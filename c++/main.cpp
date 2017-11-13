c235c57afa40dd313fb3c7d21d13b6345e1425fe
baihongwei <baihongwei@lingxi360.com>
2017-11-13 18:11:38 +0800

    Initial Commit

---
+++ c++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  c++
+//
+//  Created by zack on 2017/11/13.
+//  Copyright © 2017年 zack. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
